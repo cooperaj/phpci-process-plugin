3280adcdfc3f6859276973fa60936c062a5ee701
Mikk Tendermann <mikk150@gmail.com>
2016-02-03 17:28:58 +0200

    fix #3

--- src/Process.php
+++ src/Process.php
@@ -16,19 +16,34 @@ class Process
     public function __construct($cmd)
     {
         $this->builder = new ProcessBuilder();
-        $this->builder->setPrefix('exec');
+        
+        $this->builder->inheritEnvironmentVariables();
 
-        $this->builder->setArguments(explode(' ', $cmd));
+        $arguments=explode(' ', $cmd);
+        
+        foreach ($arguments as $key => $argument) {
+            if (preg_match('/([A-Z][A-Z0-9_-]+)=(.*)/', $argument, $matches)) {
+                $this->builder->setEnv($matches[1], $matches[2]);
+                unset($arguments[$key]);
+            }
+        }
+
+        # $this->builder->setPrefix('exec');
+
+        $this->builder->setArguments($arguments);
 
         $this->symfonyProcess = $this->builder->getProcess();
+#        var_dump($this->symfonyProcess);
+
     }
 
-    public function start()
+    public function start($callback = null)
     {
-        $this->symfonyProcess->start();
+        $proccess=$this->symfonyProcess->start($callback);
 
         // give the new process a bit of breathing room
         sleep(2);
+        return $proccess;
     }
 
     public function stop()
